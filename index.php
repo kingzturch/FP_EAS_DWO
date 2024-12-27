db31241afe3226dcce65e522dc56fafc0cb348f0
kingzturch <22082010194@student.upnjatim.ac.id>
2024-12-27 15:17:54 +0700

    perubahan 3

--- index.php
+++ index.php
@@ -22,19 +22,25 @@
 <body>
 
     <?php
+    $error_message = ''; // Menyimpan pesan error jika ada
 
-  if (!empty($_POST)) {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    if ($username == "admin" && $password == "admin") {
-      header("location:home.php");
+        // Memeriksa apakah username atau password kosong
+        if (empty($username) || empty($password)) {
+            $error_message = 'Username dan password harus diisi.';
+        } elseif ($username == "admin" && $password == "admin") {
+            // Jika username dan password benar, arahkan ke home.php
+            header("Location: home.php");
+            exit;
+        } else {
+            // Jika username atau password salah
+            $error_message = 'Username atau password salah.';
+        }
     }
-  }
-
-
-  ?>
+    ?>
 
     <div class="content">
         <div class="container">
@@ -49,50 +55,33 @@
                                 <h1><b>L O G I N</b></h1>
                                 <p class="mb-4">DASHBOARD ADVENTURE WORK</p>
                             </div>
+
+                            <!-- Menampilkan pesan error jika ada -->
+                            <?php if ($error_message): ?>
+                            <div class="alert alert-danger" role="alert">
+                                <?php echo $error_message; ?>
+                            </div>
+                            <?php endif; ?>
+
                             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                                 <p style="color: black;">Username :</p>
                                 <div class="form-group first">
                                     <label for="username">Username</label>
-                                    <input type="text" class="form-control" id="username" name="username">
-
+                                    <input type="text" class="form-control" id="username" name="username"
+                                        value="<?php echo isset($username) ? htmlspecialchars($username) : ''; ?>">
                                 </div>
-                                <p style="color: black;">Passsword :</p>
+
+                                <p style="color: black;">Password :</p>
                                 <div class="form-group last mb-4">
                                     <label for="password">Password</label>
                                     <input type="password" class="form-control" id="password" name="password">
-
                                 </div>
-                                <!-- 
-              <div class="d-flex mb-5 align-items-center">
-                <label class="control control--checkbox mb-0"><span class="caption">Remember me</span>
-                  <input type="checkbox" checked="checked"/>
-                  <div class="control__indicator"></div>
-                </label>
-                <span class="ml-auto"><a href="#" class="forgot-pass">Forgot Password</a></span> 
-              </div>
-              -->
+
                                 <input type="submit" value="Login" class="btn btn-block btn-success">
-                                <!--
-              <span class="d-block text-left my-4 text-muted">&mdash; or login with &mdash;</span>
-              
-              <div class="social-login">
-                <a href="#" class="facebook">
-                  <span class="icon-facebook mr-3"></span> 
-                </a>
-                <a href="#" class="twitter">
-                  <span class="icon-twitter mr-3"></span> 
-                </a>
-                <a href="#" class="google">
-                  <span class="icon-google mr-3"></span> 
-                </a>
-              </div>
-              -->
                             </form>
                         </div>
                     </div>
-
                 </div>
-
             </div>
         </div>
     </div>
